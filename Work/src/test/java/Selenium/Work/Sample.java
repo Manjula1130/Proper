51ad7576aeb2225183a863ed98d84c577d06eb9d
Manjula <Manjula@DESKTOP-6088UIJ>
2020-08-27 01:45:55 +0530

    New Changes

---
+++ Work/src/test/java/Selenium/Work/Sample.java
@@ -0,0 +1,14 @@
+package Selenium.Work;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class Sample {
+	public void Test()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("www.google.com");
+	}
+}
